d9981ef3e64e4ca0b706c2599e273dbf3e00bf0e
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-04-17 22:57:03 +0800

    update attendance.php now to timenow
    NOW() -> timenow

--- public_scanner/attendance.php
+++ public_scanner/attendance.php
@@ -1,9 +1,14 @@
 <?php
+
+	date_default_timezone_set("Asia/Manila");	
+
 	if(isset($_POST['faculty'])){
 		$output = array('error'=>false);
 
 		include 'conn.php';
 		include 'timezone.php';
+
+		$timenow = date("H:i:s");
 
 		$faculty_qr = $_POST['faculty'];
 		$status = $_POST['status'];
@@ -32,16 +37,17 @@
 					//updates
 					$facultyID = $row['facultyID'];
 					$sched = $row['schedule_id'];
-					$lognow = date('H:i:s');
+					$time = date("h:i:sa"); // Input time in 12-hour format
+					$time_24hrs_format = date('H:i:s', strtotime($time));
 
 					$sql = "SELECT * FROM schedules WHERE id = '$sched'";
 					$squery = $conn->query($sql);
 
 					$srow = $squery->fetch_assoc();
 					// $logstatus = ($lognow > $srow['time_in']) ? 1 : 0;
-					$logstatus = ($lognow > $srow['time_in']) ? 0 : 1;
+					$logstatus = ($srow['time_in'] > $time_24hrs_format ) ? 0 : 1;
 					//
-					$sql = "INSERT INTO attendance (facultyID, date, time_in, status) VALUES ('$facultyID', '$date_now', NOW(), '$logstatus')";
+					$sql = "INSERT INTO attendance (facultyID, date, time_in, status) VALUES ('$facultyID', '$date_now', '$timenow', '$logstatus')";
 
 					if($conn->query($sql)){
 						$output['message'] = 'Time in: '.$row['faculty_firstname'].' '.$row['faculty_lastname'].' '.'"Please take a screenshot"';
@@ -58,7 +64,7 @@
 				$sql = "SELECT * FROM attendance WHERE facultyID = '$facultyID' AND date = '$date_now' AND status = '1'";
 				$query = $conn->query($sql);
 				
-				if($query-> num_rows < 0){
+				if($query-> num_rows <= 0){
 					$output['error'] = true;
 					$output['message'] = 'Cannot Timeout. No time in.';
 				}else{
@@ -69,7 +75,7 @@
 						$output['error'] = true;
 						$output['message'] = 'You have timed out for today';
 					}else{								
-						$sql = "UPDATE attendance SET time_out = NOW(), status ='0' WHERE facultyID = '$facultyID' AND status ='1'";
+						$sql = "UPDATE attendance SET time_out = '$timenow', status ='0' WHERE facultyID = '$facultyID' AND status ='1'";
 
 						if($conn->query($sql)){
 							$output['message'] = 'Time out: '.$row['faculty_firstname'].' '.$row['faculty_lastname'].'';
